91ac539b556c24fc22a67597eaaed00ea6ac3671
ashif-reza <mdashifreza111@gmail.com>
2023-09-01 07:12:40 +0000

    Update 'src/utils/api.js'

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-export const API = "https://calm-pink-rattlesnake-garb.cyclic.app";
+// export const API = "https://calm-pink-rattlesnake-garb.cyclic.app";
+export const API = "https://api.neuralsage.ai";
 
 // export const API = "http://localhost:8000";
 
